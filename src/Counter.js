fd5a3f2aaa959c08bf89acfd02d0d28bcb5e8749
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-16 12:53:21 +0530

    set value into localStorage

--- src/Counter.js
+++ src/Counter.js
@@ -1,34 +1,33 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+const getStateFromLocalStorage = () => {
+  const storage = localStorage.getItem("counterState");
+  if (storage) return JSON.parse(storage).count;
+  return { count: 0 };
+};
+const storeStateInLocalStorage = (count) => {
+  localStorage.setItem("counterState", JSON.stringify({ count }));
+  console.log(localStorage);
+};
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-  }
-  increment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  decrement = () => {
-    if (this.state.count < 1) return null;
-    this.setState({ count: this.state.count - 1 });
-  };
-  reset = () => {
-    this.setState({ count: 0 });
-  };
-  render() {
-    return (
-      <div className="Counter">
-        <p className="count">{this.state.count}</p>
-        <section className="controls">
-          <button onClick={this.increment}>Increment</button>
-          <button onClick={this.decrement}>Decrement</button>
-          <button onClick={this.reset}>Reset</button>
-        </section>
-      </div>
-    );
-  }
-}
+const Counter = () => {
+  const [count, setCount] = useState(getStateFromLocalStorage());
+  const increment = () => setCount(count + 1);
+
+  const decrement = () => (count < 1 ? null : setCount(count - 1));
+  const reset = () => setCount(0);
+  useEffect(() => {
+    storeStateInLocalStorage(count);
+  }, [count]);
+  return (
+    <div className="Counter">
+      <p className="count">{count}</p>
+      <section className="controls">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </section>
+    </div>
+  );
+};
 
 export default Counter;
