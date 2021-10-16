e3585052826be7b41cb0b4b54d4ccd78ac4090db
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-16 11:12:36 +0530

    add methods

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  increment = () => {
+    return this.setState({ count: this.state.count + 1 });
+  };
+  decrement = () => {
+    if (this.state.count < 1) return null;
+    this.setState({ count: this.state.count - 1 });
+  };
+  reset = () => {
+    this.setState({ count: 0 });
+  };
   render() {
     return (
       <div className="Counter">
-        <p className="count">0</p>
+        <p className="count">{this.state.count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
         </section>
       </div>
     );
